e72972790a10dbf13f2d02f58ab38046436d1a8f
Anna <tutivanna2@gmail.com>
2022-07-30 20:42:51 +0300

    Add WebException

---
+++ src/main/resources/db/changelog/exception/WebException.java
@@ -0,0 +1,26 @@
+package com.company.demo.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class WebException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
+    public WebException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public WebException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public WebException(Throwable cause, HttpStatus httpStatus) {
+        super(cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
